bfb3255d6d44f8c0a893ec46f4478f48ac61523c
Matt Thomas <mmattthomas@gmail.com>
2015-07-18 14:57:28 -0700

    Initial Content

---
+++ app/main/controllers/main_controller.rb
@@ -0,0 +1,52 @@
+# By default Volt generates this controller for your Main component
+module Main
+  class MainController < Volt::ModelController
+
+    before_action :require_login, only: :index
+
+    def index
+      # Add code for when the index view is loaded
+      reset_message
+    end
+
+    def about
+      # Add code for when the about view is loaded
+    end
+
+
+    private
+    #not sure why private methods are accessible from view?
+
+    def send_message
+      store._messages << page._form
+      reset_message
+      scroll_bottom # this no work!
+    end
+
+    def reset_message
+      page._form = Message.new
+    end
+
+    def scroll_bottom
+      `
+       var newscrollHeight = $('.panel-body').attr('scrollHeight') - 20;
+       alert('newscrollHeight:' + newscrollHeight);
+       $('.panel-body').animate({ scrollTop: newscrollHeight }, 'normal');
+      `
+    end
+
+
+    # The main template contains a #template binding that shows another
+    # template.  This is the path to that template.  It may change based
+    # on the params._component, params._controller, and params._action values.
+    def main_path
+      "#{params._component || 'main'}/#{params._controller || 'main'}/#{params._action || 'index'}"
+    end
+
+    # Determine if the current nav component is the active one by looking
+    # at the first part of the url against the href attribute.
+    def active_tab?
+      url.path.split('/')[1] == attrs.href.split('/')[1]
+    end
+  end
+end
---
+++ app/main/models/message.rb
@@ -0,0 +1,4 @@
+class Message < Volt::Model
+	own_by_user
+	field :body, String
+end
